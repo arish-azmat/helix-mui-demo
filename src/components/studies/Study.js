57662d1329d714cf76c9d8abbe230a51407fcb70
Shubham Untwal <s.untwal1@saama.com>
2022-07-11 13:43:51 +0530

    title collaps

--- src/components/studies/Study.js
+++ src/components/studies/Study.js
@@ -2,6 +2,8 @@ import { Card, Grid, makeStyles } from '@mui/material'
 import React, { useState } from 'react'
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import StarIcon from '@mui/icons-material/Star';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 const columns = [
     "Study Id", "Compound", "Asset", "Therapeutic Area", "Indication", "Project", "Development Phase", "Drug Program"
 ];
@@ -9,9 +11,10 @@ const columns = [
 
 export default function Study({ study, markUnmarkAsFav }) {
     const [fav, setFav] = useState(false)
+    const [showTitle, setShowTitle] = useState(false)
 
     return (
-        <Card style={{ paddingLeft: '15px', marginTop: 15,border:'1px solid #0000001a' }}>
+        <Card style={{ paddingLeft: '15px', marginTop: 15, border: '1px solid #0000001a' }}>
             <Grid container >
                 <Grid item md={0.3} onClick={() => { setFav(!fav); markUnmarkAsFav(study) }}>
                     {fav ? <StarIcon style={{ margin: '15px 0', cursor: "pointer" }} /> : <StarBorderIcon style={{ margin: '15px 0', cursor: "pointer" }} />}
@@ -22,9 +25,24 @@ export default function Study({ study, markUnmarkAsFav }) {
                         <p><strong>{Object.values(study)[index]}</strong></p>
                     </Grid>)
                 }
-
+                <Grid item md={0.3}></Grid>
+                <Grid item md={11}  onClick={()=>setShowTitle(!showTitle)}>
+                    <p style={{
+                        fontSize: 12,
+                        cursor:'pointer',
+                        ...(showTitle ? {} : {
+                            display: "-webkit-box",
+                            "-webkit-line-clamp": "1",
+                            "-webkit-box-orient": "vertical",
+                            overflow: "hidden"
+                        })
+                    }}>{study.StudyTitle}</p>
+                </Grid>
+                <Grid item md={0.7}  onClick={()=>setShowTitle(!showTitle)}>
+                {showTitle ? <KeyboardArrowUpIcon style={{ margin: '8px 0', cursor: "pointer" }} /> : <KeyboardArrowDownIcon style={{ margin: '8px 0', cursor: "pointer" }} />}
+                </Grid>
             </Grid>
-            <p style={{ fontSize: 12 }}>{study.StudyTitle}</p>
+
         </Card>
     )
 }
